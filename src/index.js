96f942dbef597e2f299f6b5c41a2666ddaf0b600
duyle <duyle@safetrust.com>
2024-11-16 17:02:14 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './style.css';
+
+const element = document.getElementById('main');
+element.innerHTML = 'Hello Webpack';
+element.classList.add('text-primary');
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Basic Web",
+      template: 'src/index.html',
+      scriptLoading: "defer",
+    }),
+  ],
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
